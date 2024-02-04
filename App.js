1c09c2b8040e0745afd43f46069b23d4c19856c2
Milisingh00 <113784238+Milisingh00@users.noreply.github.com>
2024-02-04 19:08:47 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import About from './AboutUs/About';
+import './App.css';
+import Footer from "./Components/Footer.js";
+
+function App() {
+  return (
+    <div className="App">
+      <About/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
